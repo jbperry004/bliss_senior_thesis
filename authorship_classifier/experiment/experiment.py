de492c212b76059fba128ddae07cc344fac415be
Bliss Perry <jordanperry@college.harvard.edu>
2021-03-01 17:04:55 -0500

    added requirements

--- authorship_classifier/experiment/experiment.py
+++ authorship_classifier/experiment/experiment.py
@@ -4,7 +4,7 @@ from naive_bayes import NaiveBayes
 # bytepair_classifier = BaseClassifier(division="sents", batch_size=64, learning_rate=0.0001, epochs=15, embedding_size=64, hidden_size=32)
 # bytepair_classifier.run_bootstrap(1)
 
-naive_bayes_classifier = NaiveBayes()
-naive_bayes_classifier.eval()
-naive_bayes_classifier.predict()
+# naive_bayes_classifier = NaiveBayes()
+# naive_bayes_classifier.eval()
+# naive_bayes_classifier.predict()
 
