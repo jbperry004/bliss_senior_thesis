5ccb9968151e1dfaee1efd94ccf5c016bd049d3a
Bliss Perry <jordanperry@college.harvard.edu>
2021-03-01 17:20:43 -0500

    added config

--- authorship_classifier/experiment/experiment.py
+++ authorship_classifier/experiment/experiment.py
@@ -1,8 +1,8 @@
 from base_classifier import BaseClassifier
 from naive_bayes import NaiveBayes
 
-# bytepair_classifier = BaseClassifier(division="sents", batch_size=64, learning_rate=0.0001, epochs=15, embedding_size=64, hidden_size=32)
-# bytepair_classifier.run_bootstrap(1)
+bytepair_classifier = BaseClassifier(division="sents", batch_size=64, learning_rate=0.0001, epochs=15, embedding_size=64, hidden_size=32)
+bytepair_classifier.run_bootstrap(1)
 
 # naive_bayes_classifier = NaiveBayes()
 # naive_bayes_classifier.eval()
---
+++ scaping/scaper.py
@@ -0,0 +1,63 @@
+import requests
+import json
+from bs4 import BeautifulSoup, NavigableString
+from collections import defaultdict
+
+url = "/cgi-bin/perseus/citequery3.pl?dbname=GreekDec20&getid=0&query=Pl.%20Cri.%2043a"
+r = requests.get("http://artflsrv02.uchicago.edu/" + url)
+outer_soup = BeautifulSoup(r.text, "html.parser")
+
+words = defaultdict(list)
+section = None
+next_url = outer_soup.find("td", align="right").contents[0]["href"]
+while next_url != url:
+    tag = outer_soup.find("div", id="perseuscontent").contents[0]
+
+    while (True in ("id" in parent.attrs and parent["id"] == "perseuscontent" for parent in tag.parents)):
+        if tag.name == "w":
+            print("LEMMA")
+            word_id = tag["id"]
+            word = tag.string
+            inner_url = "http://artflsrv02.uchicago.edu/cgi-bin/perseus/morph.pl?lang=greek&id=" +  word_id
+            inner_r = requests.get(inner_url)
+            inner_soup = BeautifulSoup(inner_r.text, "html.parser")
+
+            lemma = inner_soup.find("th", class_="lemma").string
+            parse = inner_soup.find("tr", class_=["parse probable parserow", "parse disambiguated parserow"]).contents[1].string
+
+            word_entry = {"word": word, "lemma": lemma, "parse": parse}
+            words[section].append(word_entry)
+
+        elif isinstance(tag, NavigableString):
+            print("CHAR")
+            if str(tag) != " ":
+                words[section].append({"word": str(tag)})
+
+        elif ("class" in tag.attrs and "mstonecustom" in tag["class"]):
+            print("MILESTONE")
+            print(words[section])
+            section = tag["id"]
+
+        elif len(tag.contents) > 0:
+            print(tag.contents)
+            print("STEPPING DOWN")
+            print(tag)
+            if (tag.name == 'p') or ("class" in tag.attrs and "bibl" not in tag["class"]):
+                tag = tag.contents[0]
+                continue
+
+        while tag.next_sibling == None:
+            print("STEPPING UP")
+            print(tag.parent)
+            tag = tag.parent
+
+        print("STEPPING SIDEWAYS")
+        tag = tag.next_sibling
+
+    url = outer_soup.find("td", align="right").contents[0]["href"]
+    r = requests.get("http://artflsrv02.uchicago.edu/" + url)
+    outer_soup = BeautifulSoup(r.text, "html.parser")
+    # next_url = outer_soup.find("td", align="right").contents[0]["href"]
+
+with open('lemmatized_crito.json', 'w') as fp:
+    json.dump(words, fp)
